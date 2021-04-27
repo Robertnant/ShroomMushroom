5f156b4945436b19ac70bbbc8b5b0c6f896ee0a9
Manon LOVELAND <manon.loveland@epita.fr>
2021-04-27 20:29:53 +0200

    registration folder
    Signed-off-by: Manon LOVELAND <manon.loveland@epita.fr>

--- design/Registration/reg_page.c
+++ design/Registration/reg_page.c
@@ -1,34 +1,48 @@
 #include <gtk/gtk.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-//ADD a text label which will display an error if the user puts in something wrong
 
 GtkLabel *textlabel;
 
 typedef struct 
 {
     GtkButton *register_now_button;
-    //GtkEntry *TextEntry;
-    //GtkTextBuffer *name_textbuffer;
-    //GtkLabel *textlabel;
     GtkEntry *username;
     GtkEntry *phonenumber;
+    GtkLabel *error_label;
     
 } app_widgets;
 
 app_widgets *curr;
 
+
 void on_register_now_button_clicked(GtkWidget *widget, gpointer data)
 {
 	printf("register_now_button pressed\n");
-	//char buffer[16];
+	app_widgets *arg = data; 
 
-	//make sure the name and number are words and numbers 
-	//username max of 16 characters , at least 5 characters 
-	//number 10 characters long and only digits
+	char buffer_for_phone[11];
+	strcpy(buffer_for_phone, gtk_entry_get_text(arg->phonenumber)); //might need to free it 
+
+	for (int i = 0; i < 10; i++){
+		if (!(buffer_for_phone[i] >= '0' && buffer_for_phone[i] <= '9')){
+			printf("Wrong Format : Detected character %c\n", buffer_for_phone[i]);
+			break;
+		}
+	}
+
+	if (strlen(buffer_for_phone) != 10){
+		printf("Wrong length\n");
+	}
+
+	char buffer_for_username[17];
+	strcpy(buffer_for_username, gtk_entry_get_text(arg->username));
+	
+	if (strlen(buffer_for_username) < 6){
+		printf("Wrong length username\n");
+	}
+
 }
 
 static void destroy(GtkWidget *widget, gpointer data){
@@ -48,14 +62,13 @@ int main()
     gtk_init(NULL, NULL);
 
     builder_registration = gtk_builder_new_from_file("./reg_page.glade");
-
     window_registration = GTK_WIDGET(gtk_builder_get_object(builder_registration, "reg_page"));
-
     widgets_registration->register_now_button= GTK_BUTTON(gtk_builder_get_object(builder_registration, "register_now_button"));
 
-    //widgets->TextEntry = GTK_ENTRY(gtk_builder_get_object(builder, "TextEntry"));
+    widgets_registration->phonenumber = GTK_ENTRY(gtk_builder_get_object(builder_registration, "number_textentry"));
+    widgets_registration->username = GTK_ENTRY(gtk_builder_get_object(builder_registration, "username_textentry"));
     //widgets_registration->name_textbuffer = GTK_TEXT_BUFFER(gtk_builder_get_object(builder_registration, "name_textbuffer"));
-    //widgets->textlabel = GTK_LABEL(gtk_builder_get_object(builder, "textlabel"));
+    //widgets->error_label = GTK_LABEL(gtk_builder_get_object(builder, "textlabel"));
 
     g_signal_connect(widgets_registration->register_now_button, "clicked", G_CALLBACK(on_register_now_button_clicked), (gpointer) widgets_registration);
     g_signal_connect(window_registration, "destroy", G_CALLBACK(destroy), (gpointer) widgets_registration);
